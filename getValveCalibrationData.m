7b65b5268a56dc6dcad493ab78ebad04b952f35d
Michael Krumin <mkrumin@gmail.com>
2017-11-06 17:53:08 +0000

    Updated valve calibration

--- getValveCalibrationData.m
+++ getValveCalibrationData.m
@@ -8,8 +8,11 @@ switch lower(rigName)
 %         water = [0.002, 0.004];
 %         dur = [0.07, 0.115];
         % calbration on 2017-08-13
-        water = [0.0014, 0.0023, 0.0032, 0.004, 0.0056, 0.0072];
-        dur = [0.0475, 0.07, 0.0925, 0.115, 0.16, 0.205];
+%         water = [0.0014, 0.0023, 0.0032, 0.004, 0.0056, 0.0072];
+%         dur = [0.0475, 0.07, 0.0925, 0.115, 0.16, 0.205];
+        % calbration on 2017-10-04
+        water = [1.98, 3, 4.12, 6.3]*1e-3;
+        dur = [0.069, 0.098, 0.1281, 0.1998];
     otherwise
         msg = ...
             sprintf('No valve calibration data for rig ''%s'', loading default values...\n', upper(rigName));
