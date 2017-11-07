c46e88d1524b2f509e4cb063b315730a592205ce
Michael Krumin <mkrumin@gmail.com>
2017-11-07 19:23:51 +0000

    Generating list of stimulation locations

--- getOptiStimList.m
+++ getOptiStimList.m
@@ -1,30 +1,41 @@
 function list = getOptiStimList()
-
+%%
 list = struct;
 
+% definte all the locations with laser Powers
 ML = [-1.7, 1.7];
 AP = [-2, -2];
 laserPower = [2 2];
 
+% define possible stimulation types
+% stimType = {'POSITION', 'POSITION', 'POSITION'};
+% onset = [0 35 70]; stimulating during the first, second, or third third
+% of the maze
+% offset = [35 70 110];
 stimType = {'POSITION'};
 onset = [0];
 offset = [110];
 
-
 nPoints = length(ML);
 nTypes = length(stimType);
 
+% make a list with all the necessary combinations
+% for each stimulation type we go through all the locations, which can be
+% 'on' or 'off' independently of each other
+% 'on' or 'off' state will be controlled by the laser power
+
+nStates = nPoints^2;
+states = dec2bin(0:nStates-1, nPoints) == '1';
 iStim = 0;
 for iType = 1:nTypes
-    for iPoint = 1:nPoints
-        for jPoint = iPoint:nPoints
-            iStim = iStim + 1;
+    for iState = 1:nStates
+        
+        iStim = iStim + 1;
         list(iStim).ML = ML;
         list(iStim).AP = AP;
-        list(iStim).laserPower = 
+        list(iStim).laserPower = laserPower.*states(iState, :);
         list(iStim).stimType = stimType{iType};
         list(iStim).onset = onset(iType);
         list(iStim).offset = offset(iType);
-        end
     end
 end
